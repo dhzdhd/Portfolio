207bc858a6317384d0c685f056c9fc11e7810820
dhzdhd <ragas5432100@gmail.com>
2021-12-09 11:35:26 +0530

    Added docker config and second page.

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,17 @@
-FROM npm
+FROM mcr.microsoft.com/dotnet/sdk:6.0
+
+WORKDIR /usr/app
+COPY . /usr/app/
+
+EXPOSE 8080
+
+RUN curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
+RUN apt-get install -y nodejs
+
+ENV NPM_CONFIG_PREFIX=/home/node/.npm-global
+
+RUN dotnet tool restore
+RUN npm install
+
+ENTRYPOINT [ "npm" ]
+CMD [ "start" ]
--- src/App.fs
+++ src/App.fs
@@ -1,14 +1,40 @@
 module App
 
 open Browser.Dom
+// open Octokit
+
+// open FsHttp
+// open FsHttp.DslCE
+
+// module private repositories =
+//     let getBio () =
+//         http {
+//             POST "https://reqres.in/api/users"
+//             CacheControl "no-cache"
+//             body
+//             json """
+//             {
+//                 "name": "morpheus",
+//                 "job": "leader"
+//             }
+//             """
+//         } 
+//         |> Response.toJson 
+//     //     let client = GitHubClient (ProductHeaderValue "dhzdhd")
+//     //     let a = client.User.Get "dhzdhd"
+//     //     a.Result.Bio
+//     ()
 
 // Mutable variable to count the number of times we clicked the button
 let mutable count = 0
 
 // Get a reference to our button and cast the Element to an HTMLButtonElement
-let myButton = document.querySelector(".menu-button") :?> Browser.Types.HTMLButtonElement
+let myButton = document.querySelector(".down-button") :?> Browser.Types.HTMLButtonElement
 
 // Register our listener
 myButton.onclick <- fun _ ->
-    count <- count + 1
-    myButton.innerText <- $"You clicked: %i{count} time(s)"
+    // let e = repositories.getBio ()
+    // printfn $"{e}"
+    ()
+
+
