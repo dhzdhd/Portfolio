529154301061ae13f0247ca6e6e11cb85efcc138
dhzdhd <ragas5432100@gmail.com>
2021-12-09 12:50:13 +0530

    Add transform animation effect.

--- src/App.fs
+++ src/App.fs
@@ -31,6 +31,7 @@ let mutable count = 0
 // Get a reference to our button and cast the Element to an HTMLButtonElement
 let myButton = document.querySelector(".down-button") :?> Browser.Types.HTMLButtonElement
 
+
 // Register our listener
 myButton.onclick <- fun _ ->
     // let e = repositories.getBio ()
--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,18 @@ module.exports = {
         port: 8080,
     },
     module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ],
     }
 }
