3c18e9392f26a8b48d71afabe1a63e5b20ab3ef6
dhzdhd <ragas5432100@gmail.com>
2021-12-10 15:40:43 +0530

    Added random text.

--- src/App.fs
+++ src/App.fs
@@ -1,34 +1,37 @@
-module E
+module App
 
 open Browser.Dom
 open Fetch
 open Thoth.Json
 
-module private facts =
-    let update text =
-        let todayFact = document.querySelector(".today-fact-text") :?> Browser.Types.HTMLHeadingElement
-        todayFact.innerText <- text
+module private Facts =
+    let updateTodayText text =
+        let todayFactText = document.querySelector(".today-fact-text") :?> Browser.Types.HTMLHeadingElement
+        todayFactText.innerText <- text
         ()
 
-    let getTodayFact (url:string) =
+    let updateRandomText text =
+        let randomFactText = document.querySelector(".random-fact-text") :?> Browser.Types.HTMLHeadingElement
+        randomFactText.innerText <- text
+        ()
+
+    let getFact (url:string) (factType:string) =
         fetch url []
         |> Promise.bind (fun res -> res.text())
         |> Promise.map (fun txt -> 
             let decoded = Decode.Auto.fromString<{|Text: string|}> (txt, caseStrategy = CamelCase)
-            match decoded with
-            | Ok resp ->  
-                update resp.Text
-            | Error decodingError -> 
-                printfn "Error"
+            match decoded, factType with
+            | Ok resp, "today" -> updateTodayText resp.Text
+            | Ok resp, "random" -> updateRandomText resp.Text
+            | Error decodingError, "today" -> updateTodayText decodingError
+            | Error decodingError, "random" -> updateRandomText decodingError
         )
         |> ignore
-    ()
 
-let myButton = document.querySelector(".down-button") :?> Browser.Types.HTMLButtonElement
+Facts.getFact "https://uselessfacts.jsph.pl/today.json?language=en" "today"
 
-
-facts.getTodayFact "https://uselessfacts.jsph.pl/today.json?language=en"
-// myButton.onclick <- fun _ ->
-//     repositories.getBio ()
+let myButton = document.querySelector(".get-fact") :?> Browser.Types.HTMLButtonElement
+myButton.onclick <- fun _ ->
+    Facts.getFact "https://uselessfacts.jsph.pl/random.json?language=en" "random"
 
 
